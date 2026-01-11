c62615df537f8c756ce64bc778030569ff0f6508
pinilDissanayaka <pinildissanayaka@gmail.com>
2026-01-11 12:00:23 +0530

    change config

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 SAMPLE_RATE = 22050
 CHANNELS = 1
-CHUNK_DURATION_MS = 8000  # 8 seconds
+
+# 🔽 ADD THESE
+MIN_SEGMENT_SEC = 2.0
+MAX_SEGMENT_SEC = 10.0
+
 LANGUAGE = "si"
 
 RAW_AUDIO_DIR = "data/raw_audio"
 WAV_AUDIO_DIR = "data/wav_audio"
 CHUNKS_WAV_DIR = "data/chunks/wavs"
-CHUNKS_TEXT_DIR = "data/chunks/text"
 FINAL_WAVS_DIR = "data/final_dataset/wavs"
 METADATA_PATH = "data/final_dataset/metadata.csv"
 
 WHISPER_MODEL = "large-v3"
-DEVICE = "cuda"  # change to "cpu" if no GPU
+DEVICE = "cuda"
